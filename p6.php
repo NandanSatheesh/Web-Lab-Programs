c75451924bc208b624fd5bc389c24bf10770afd4
bhaskars2197 <33758173+bhaskars2197@users.noreply.github.com>
2018-11-20 21:23:26 +0530

    Add files via upload

---
+++ p6.php
@@ -0,0 +1,29 @@
+<?php
+
+	echo "<h1 style='text-align:center'> Refresh Page </h1>" ;
+	/*
+	$name = 'counter.txt' ;
+	$file = fopen($name,'r') ;
+	$hits = fscanf($file,'%d');
+
+	fclose($file) ;
+
+	file_get_contents($name) ;
+
+	$hits[0]++ ;
+	$file = fopen($name,'w') ;
+	fprintf($file,'%d',$hits[0]);
+	fclose($file) ;
+	
+
+
+	echo "<h3 style='text-align:center'>Total number of views :".$hits[0]." </h3>" ;
+
+	*/
+	$name = 'counter.txt' ;
+	$c = file_get_contents($name) ;
+
+	echo $c ;
+
+	file_put_contents($name, $c+1);
+?>
