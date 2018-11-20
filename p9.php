f8448d81f3ad0a4822c95b1ed354c424120d893f
bhaskars2197 <33758173+bhaskars2197@users.noreply.github.com>
2018-11-20 21:22:02 +0530

    Add files via upload

---
+++ p9.php
@@ -0,0 +1,32 @@
+<?php
+$states = "Mississippi Alabama Texas Massachusetts Kansas";
+$statesArray = [];
+$states1 = explode(' ',$states);
+echo "Original Array :<br>"; 
+foreach ( $states1 as $i => $value )
+print("STATES[$i]=$value<br>"); 
+foreach($states1 as $state) 
+{
+	if(preg_match( '/xas$/', ($state)))
+		$statesArray[0] = ($state);
+}
+foreach($states1 as $state) 
+{
+	if(preg_match('/^k.*s$/i', ($state)))
+		$statesArray[1] = ($state);
+}
+
+foreach($states1 as $state) 
+{
+	if(preg_match('/^M.*s$/', ($state)))
+		$statesArray[2] = ($state);
+}
+foreach($states1 as $state)
+{
+	if(preg_match('/a$/', ($state)))
+		$statesArray[3] = ($state);
+}
+echo "<br><br>Resultant Array :<br>"; 
+foreach ( $statesArray as $array => $value )
+print("STATES[$array]=$value<br>");
+?>
