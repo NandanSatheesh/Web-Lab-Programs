a4c8afa42e5f022bfa4078d2c1922aa32fd27bd6
Nandan Satheesh <nandan.199776@gmail.com>
2018-12-01 17:22:01 +0530

    Update p6.php

--- p6.php
+++ p6.php
@@ -1,6 +1,7 @@
 <?php
 
 	echo "<h1 style='text-align:center'> Count Page </h1>" ;
+
 	/* Alternate method - Not recommended
 	
 	$name = 'counter.txt' ;
@@ -22,10 +23,11 @@
 	*/
 
 	// This is simple XD
+
 	$name = 'counter.txt' ;
 	$c = file_get_contents($name) ;
-
 	echo "The number of users visited : ".$c ;
-
 	file_put_contents($name, $c+1);
+
+
 ?>
