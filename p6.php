1e27084bd0f890586087de943ce9e3de823c9e99
Nandan Satheesh <nandan.199776@gmail.com>
2018-12-01 17:21:29 +0530

    Update p6.php

--- p6.php
+++ p6.php
@@ -1,7 +1,8 @@
 <?php
 
 	echo "<h1 style='text-align:center'> Count Page </h1>" ;
-	/*alternate method
+	/* Alternate method - Not recommended
+	
 	$name = 'counter.txt' ;
 	$file = fopen($name,'r') ;
 	$hits = fscanf($file,'%d');
@@ -16,10 +17,11 @@
 	fclose($file) ;
 	
 
-
 	echo "<h3 style='text-align:center'>Total number of views :".$hits[0]." </h3>" ;
 
 	*/
+
+	// This is simple XD
 	$name = 'counter.txt' ;
 	$c = file_get_contents($name) ;
 
