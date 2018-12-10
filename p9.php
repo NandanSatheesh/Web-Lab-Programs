350cf38839d70f6beb09a9e222aa9bfcafb5e1e9
Nandan Satheesh <nandan.199776@gmail.com>
2018-12-10 13:31:00 +0530

    Changed Regex - Update p9.php
    Changed Regex - Update p9.php

--- p9.php
+++ p9.php
@@ -7,7 +7,7 @@ foreach ( $states1 as $i => $value )
 print("STATES[$i]=$value<br>"); 
 foreach($states1 as $state) 
 {
-	if(preg_match( '/xas$/', ($state)))
+	if(preg_match( '/.*xas$/', ($state)))
 		$statesArray[0] = ($state);
 }
 foreach($states1 as $state) 
